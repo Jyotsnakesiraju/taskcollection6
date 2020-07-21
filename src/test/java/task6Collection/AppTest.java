75af554f93db9cd9aeca5c8c18182ee8b609b86c
Jyotsnakesiraju <jyotsnasri1@gmail.com>
2020-07-21 15:46:57 +0530

    commit message

---
+++ src/test/java/task6Collection/AppTest.java
@@ -0,0 +1,21 @@
+package task6Collection;
+
+import junit.framework.TestCase;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest extends TestCase {
+	  /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/task6Collection/apptest6.java
@@ -0,0 +1,14 @@
+package task6Collection;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class apptest6 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
